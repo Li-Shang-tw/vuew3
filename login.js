e8f90757903935099e5900daa8ecf3e4298e1970
Li-Shang-tw <41882910+Li-Shang-tw@users.noreply.github.com>
2021-05-27 09:55:35 +0800

    import vue createApp

--- login.js
+++ login.js
@@ -2,6 +2,8 @@
 //api
 const url ='https://vue3-course-api.hexschool.io/';
 const path ="shang";
+//引入vue createApp
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.esm-browser.js';
 
 const app = Vue.createApp({
   data(){
